10122d2f1628b563c978a7fdd5fd94d40c6aa222
Shivam-Chamoli <shivamchamoli99@gmail.com>
2022-01-05 15:03:07 +0530

    First Comit
    Project Structure Successfully prepared

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Article, Brand, Cta, Feature, Navbar } from "./components";
+import {
+  Blog,
+  Header,
+  Footer,
+  WhatGpt3,
+  Features,
+  Possibility,
+} from "./containers";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App">
+      <h1> App </h1>
+      <Header />
+      <Footer />
+      <Blog />
+      <WhatGpt3 />
+      <Features />
+      <Possibility />
+      <Article />
+      <Brand />
+      <Feature />
+      <Cta />
+      <Navbar />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/feature/Feauture.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./feature.css";
+
+const Feautre = () => {
+  return <div>Feautre</div>;
+};
+
+export default Feautre;
