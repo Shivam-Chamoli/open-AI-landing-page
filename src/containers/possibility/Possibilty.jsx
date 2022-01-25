68d20becf6c807c3a43b31c1209b083a56939dab
Shivam-Chamoli <shivamchamoli99@gmail.com>
2022-01-25 07:17:58 +0530

    Sixth Commit
    Completed the Possibility Section

--- src/containers/possibility/Possibilty.jsx
+++ src/containers/possibility/Possibilty.jsx
@@ -6,15 +6,22 @@ import possibilityImg from "../../assets/possibility.png";
 const Possibility = () => {
   return (
     <div className="gpt3__possibility section__padding" id="possibility">
-      {/* <div className="gpt3__possibility-image">
+      <div className="gpt3__possibility-image">
         <img src={possibilityImg} alt="possibility asset" />
       </div>
       <div className="gpt3__possibility-content">
         <h4>Request Early Access to get started</h4>
-        <h1 className="gradient-text">
+        <h1 className="gradient__text">
           The Possibilities are beyond your Imagination
         </h1>
-      </div> */}
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <h4>Request Early Access to get started</h4>
+      </div>
     </div>
   );
 };
